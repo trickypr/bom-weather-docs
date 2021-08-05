e1e578371d41216db5366870b132a57574de1507
trickypr <trickypr@icloud.com>
2021-08-05 18:55:53 +1000

    🚀 Remoge gh pages deployment

--- web/next.config.js
+++ web/next.config.js
@@ -1,9 +1,4 @@
-const isProd = process.env.NODE_ENV === 'production'
-const prodRootPath = 'https://cdn.statically.io/gh/trickypr/bom-weather-docs/gh-pages/'
-
 module.exports = {
-	assetPrefix: isProd ? prodRootPath : '/',
-	basePath: isProd ? '/bom-weather-docs' : '',
 	webpack: (config) => {
 		return Object.assign({}, config, {
 			module: Object.assign({}, config.module, {
