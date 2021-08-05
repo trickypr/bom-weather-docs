c966f0a7f2ef0be44ed523ad6dfb2d4cf4283adf
trickypr <trickypr@icloud.com>
2021-08-05 18:25:54 +1000

    💄 Use dark syntax highlighting

--- web/components/CodeHighlighter.jsx
+++ web/components/CodeHighlighter.jsx
@@ -1,13 +1,13 @@
 import { PureComponent } from 'react'
 import { Prism as SyntaxHighlighter } from "react-syntax-highlighter"
-import { coy } from "react-syntax-highlighter/dist/cjs/styles/prism"
+import { okaidia } from "react-syntax-highlighter/dist/cjs/styles/prism"
 
 class CodeHighlighter extends PureComponent {
   render() {
     const { language, value } = this.props
 
     return (
-      <SyntaxHighlighter language={language} style={coy}>
+      <SyntaxHighlighter language={language} style={okaidia}>
         {value}
       </SyntaxHighlighter>
     )
